8b886588568713c1683ae785e99bf750e7cb32ee
Tatevik <tatevikg1@gmail.com>
2025-03-25 22:33:31 +0400

    ISSUE-338: add configs + move around check

--- plugins/simplesaml/settings.php
+++ plugins/simplesaml/settings.php
@@ -7,5 +7,7 @@ return array (
   'saml_entity_id' => 'phplisttest',
   'saml_trusted_url_domains' => 'localhost',
   'saml_session_cookie_domain' => '.localhost',
-  'saml_session_savepath' => '/var/lib/php/sessions',
+  'saml_session_save_path' => '/var/lib/php/sessions',
+  'saml_secret_salt' => 'defaultsecretsalt',
+  'saml_admin_password' => '1234',
 );
